f4e4802f1935e2cc5c70e947f6a3a975907c0be9
JohnPaul <ssebwanajohnpaul@gmail.com>
2026-04-02 00:30:33 +0300

    added_separation_pages

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('programs/', views.programs, name='programs'),
+    path('partnerships/', views.partnerships, name='partnerships'),
+    path('donate/', views.donate, name='donate'),
+    path('contact/', views.contact, name='contact'),
 ]
--- main/views.py
+++ main/views.py
@@ -3,3 +3,21 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'main/home.html')
+def about(request):
+    return render(request, 'main/about.html')
+
+
+def programs(request):
+    return render(request, 'main/programs.html')
+
+
+def partnerships(request):
+    return render(request, 'main/partnerships.html')
+
+
+def donate(request):
+    return render(request, 'main/donate.html')
+
+
+def contact(request):
+    return render(request, 'main/contact.html')
